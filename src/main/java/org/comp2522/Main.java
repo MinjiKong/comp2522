4c1a2bbbbd20326e9c52d097f05166eb867e6a1b
Wendy Kong <mkong16@my.bcit.ca>
2022-09-09 13:16:45 -0700

    fixed the style errors

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,10 @@
 package org.comp2522;
 
+/**
+ * Prints out my favourite animal.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hamster");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hamster");
+  }
 }
